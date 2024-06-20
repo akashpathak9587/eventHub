a0bee4686e574af4cb92dd19802fcf3377a26857
Akash Pathak <akashpathak@Akashs-MacBook-Air.local>
2024-06-20 18:40:38 +0530

    first commit

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,14 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+    clerkId: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    username: { type: String, unique: true, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    photo: { type: String, required: true },
+})
+
+const User = models.User || model('User', UserSchema);
+
+export default User
